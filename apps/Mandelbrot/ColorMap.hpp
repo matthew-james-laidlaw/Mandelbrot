ad04ac0059fddf832abcc9f8d0f478d82292dce7
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2025-03-10 12:44:22 -0700

    reorg

--- apps/Mandelbrot/ColorMap.hpp
+++ apps/Mandelbrot/ColorMap.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-#include "ColorMapData/Palette.hpp"
+#include "ColorMapData/Magma.hpp"
+#include "ColorMapData/Twilight.hpp"
+#include "ColorMapData/Viridis.hpp"
 
 #include <string>
+#include <iostream>
 
 enum class Colormap
 {
@@ -11,5 +14,38 @@ enum class Colormap
     Viridis,
 };
 
-auto GetColormapByName(std::string const& name) -> Colormap;
-auto GetColormapPalette(Colormap colormap) -> Palette;
+auto GetColormapByName(std::string const& name) -> Colormap
+{
+    if (name == "magma")
+    {
+        return Colormap::Magma;
+    }
+    else if (name == "twilight")
+    {
+        return Colormap::Twilight;
+    }
+    else if (name == "viridis")
+    {
+        return Colormap::Viridis;
+    }
+    else
+    {
+        std::cerr << "no colormap (or invalid colormap) requested, defaulting to magma" << std::endl;
+        return Colormap::Magma;
+    }
+}
+
+auto GetColormapPalette(Colormap colormap) -> Palette
+{
+    switch (colormap)
+    {
+    case Colormap::Magma:
+        return magma256;
+    case Colormap::Twilight:
+        return twilight256;
+    case Colormap::Viridis:
+        return viridis256;
+    default:
+        return magma256;
+    }
+}
